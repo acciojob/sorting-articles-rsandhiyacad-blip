8d1ab229d3ed3f9514b0805125dbbca2ba861a3b
rsandhiyacad-blip <rsandhiyacad@gmail.com>
2026-03-05 09:45:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const bands = [
+  "Anywhere But Here",
+  "The Bled",
+  "Counterparts",
+  "The Devil Wears Prada",
+  "The Midway State",
+  "Norma Jean",
+  "Oh, Sleeper",
+  "An Old Dog",
+  "Pierce the Veil",
+  "The Plot in You",
+  "Say Anything"
+];
+
+const ul = document.getElementById("bandList");
+
+bands.forEach(function(band) {
+  const li = document.createElement("li");
+  li.textContent = band;
+  ul.appendChild(li);
+});
